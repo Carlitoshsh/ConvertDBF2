84cd4347d5094986c0786e8c11e140be9e961dae
Carlos Alfonso Gómez Hernandez <caragomezher@unal.edu.co>
2018-08-23 17:52:21 -0500

    Agregar archivos de proyecto.

---
+++ ConvertDBF/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace ConvertDBF
+{
+    partial class MAICO
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MAICO));
+            this.consultar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // consultar
+            // 
+            this.consultar.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.consultar.Location = new System.Drawing.Point(71, 123);
+            this.consultar.Name = "consultar";
+            this.consultar.Size = new System.Drawing.Size(164, 23);
+            this.consultar.TabIndex = 0;
+            this.consultar.Text = "Consultar";
+            this.consultar.UseVisualStyleBackColor = true;
+            this.consultar.Click += new System.EventHandler(this.consultar_Click);
+            // 
+            // MAICO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.consultar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MAICO";
+            this.Text = "Pruebas Audiometria MAICO";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button consultar;
+    }
+}
+
---
+++ ConvertDBF/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ConvertDBF
+{
+    public partial class MAICO : Form
+    {
+        public MAICO()
+        {
+            InitializeComponent();
+        }
+
+        private void consultar_Click(object sender, EventArgs e)
+        {
+            string constr = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=D:\OneDrive - Olimpia Management\Escritorio;Extended Properties=dBASE IV;Database=Escritorio";
+            using (OleDbConnection con = new OleDbConnection(constr))
+            {
+                var sql = "select PEGELRT from ton;";
+                OleDbCommand cmd = new OleDbCommand(sql, con);
+                con.Open();
+                DataSet ds = new DataSet(); ;
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                da.Fill(ds);
+            }
+        }
+    }
+}
