a10068b109353acc0a02e7cf4f83c8aa3fb2b9b3
Carlos Alfonso Gómez Hernandez <caragomezher@unal.edu.co>
2018-08-24 11:41:43 -0500

    Todo

--- ConvertDBF/Form1.Designer.cs
+++ ConvertDBF/Form1.Designer.cs
@@ -30,28 +30,104 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MAICO));
             this.consultar = new System.Windows.Forms.Button();
+            this.abrirPrueba = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.apellido = new System.Windows.Forms.TextBox();
+            this.documento = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // consultar
             // 
+            this.consultar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.consultar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.consultar.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.consultar.Location = new System.Drawing.Point(71, 123);
+            this.consultar.Location = new System.Drawing.Point(197, 166);
             this.consultar.Name = "consultar";
-            this.consultar.Size = new System.Drawing.Size(164, 23);
+            this.consultar.Size = new System.Drawing.Size(164, 45);
             this.consultar.TabIndex = 0;
             this.consultar.Text = "Consultar";
-            this.consultar.UseVisualStyleBackColor = true;
+            this.consultar.UseVisualStyleBackColor = false;
             this.consultar.Click += new System.EventHandler(this.consultar_Click);
+            // 
+            // abrirPrueba
+            // 
+            this.abrirPrueba.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.abrirPrueba.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.abrirPrueba.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.abrirPrueba.Location = new System.Drawing.Point(12, 166);
+            this.abrirPrueba.Name = "abrirPrueba";
+            this.abrirPrueba.Size = new System.Drawing.Size(164, 45);
+            this.abrirPrueba.TabIndex = 1;
+            this.abrirPrueba.Text = "Abrir prueba";
+            this.abrirPrueba.UseVisualStyleBackColor = false;
+            this.abrirPrueba.Click += new System.EventHandler(this.abrirPrueba_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(95, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(178, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.documento);
+            this.groupBox1.Controls.Add(this.apellido);
+            this.groupBox1.Controls.Add(this.nombre);
+            this.groupBox1.Location = new System.Drawing.Point(12, 81);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(349, 60);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Paciente";
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(7, 22);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(100, 22);
+            this.nombre.TabIndex = 0;
+            // 
+            // apellido
+            // 
+            this.apellido.Location = new System.Drawing.Point(124, 22);
+            this.apellido.Name = "apellido";
+            this.apellido.Size = new System.Drawing.Size(100, 22);
+            this.apellido.TabIndex = 1;
+            // 
+            // documento
+            // 
+            this.documento.Location = new System.Drawing.Point(243, 22);
+            this.documento.Name = "documento";
+            this.documento.Size = new System.Drawing.Size(100, 22);
+            this.documento.TabIndex = 2;
             // 
             // MAICO
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(375, 236);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.abrirPrueba);
             this.Controls.Add(this.consultar);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MAICO";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pruebas Audiometria MAICO";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -59,6 +135,12 @@
         #endregion
 
         private System.Windows.Forms.Button consultar;
+        private System.Windows.Forms.Button abrirPrueba;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox documento;
+        private System.Windows.Forms.TextBox apellido;
+        private System.Windows.Forms.TextBox nombre;
     }
 }
 
--- ConvertDBF/Form1.cs
+++ ConvertDBF/Form1.cs
@@ -1,35 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.OleDb;
+using System.IO;
+using System.Diagnostics;
 
 namespace ConvertDBF
 {
     public partial class MAICO : Form
     {
+        public string carpeta;
         public MAICO()
         {
             InitializeComponent();
         }
 
+        private void conectarConBaseDeDatosMAICO(string selectedPath, string sql)
+        {
+            try
+            {
+                string constr = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + selectedPath + ";Extended Properties=dBASE IV;";
+                using (OleDbConnection con = new OleDbConnection(constr))
+                {
+                    OleDbCommand cmd = new OleDbCommand(sql, con);
+                    con.Open();
+                    DataSet ds = new DataSet("Resultados");
+                    OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                    da.Fill(ds);
+                    MessageBox.Show(ds.GetXml(), "Datos");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+
+        }
+
+        private void abrirCarpeta(string sql)
+        {
+            if (carpeta != null) {
+                using (var fbd = new FolderBrowserDialog())
+                {
+                    DialogResult result = fbd.ShowDialog();
+
+                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        carpeta = fbd.SelectedPath;
+                    }
+                }
+            }
+            conectarConBaseDeDatosMAICO(carpeta, sql);
+            
+        }
+
         private void consultar_Click(object sender, EventArgs e)
         {
-            string constr = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=D:\OneDrive - Olimpia Management\Escritorio;Extended Properties=dBASE IV;Database=Escritorio";
-            using (OleDbConnection con = new OleDbConnection(constr))
-            {
-                var sql = "select PEGELRT from ton;";
-                OleDbCommand cmd = new OleDbCommand(sql, con);
-                con.Open();
-                DataSet ds = new DataSet(); ;
-                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
-                da.Fill(ds);
-            }
+            var sql = "select PEGELRT from ton where IDPATAUD='"+documento.Text+"';";
+            abrirCarpeta(sql);
         }
+
+        private void editarArchivo() {
+            string datosDeUsuario = @"C:\ProgramData\Maico\ma42\whf.in";
+            string[] lines = File.ReadAllLines(datosDeUsuario);
+            lines[0] = documento.Text; //Documento
+            lines[1] = apellido.Text;
+            lines[2] = nombre.Text;
+            lines[3] = "25.12.1994";
+            File.WriteAllLines(datosDeUsuario, lines);
+        }
+
+        private void abrirPrueba_Click(object sender, EventArgs e)
+        {
+            var sql = "insert into PATIENT values("+
+                    "'"+documento.Text+ "', '" + documento.Text + "', null, '" + apellido.Text + "', '" + nombre.Text + "'," +
+                    "null, null, null, null, '1994-12-25', '2018-08-08', 1,"+
+                    "null, null, null, null, null, null, null, null)";
+            abrirCarpeta(sql);
+            editarArchivo();
+
+            ProcessStartInfo infoProceso = new ProcessStartInfo();
+            infoProceso.FileName = @"C:\Program Files (x86)\MAICO\MA42\ma42i.exe";
+            Process.Start(infoProceso);
+
+        }
+
     }
 }
